13c13e39ce7cfee19d8a7a91bc072f2f5ff83108
Lucky Ivanius <luckzivanius@gmail.com>
2026-09-09 10:12:24 +0700

    chore: use ERC8183 instead of WithAuthorization

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {ERC8183WithAuthorization} from "../contracts/ERC8183WithAuthorization.sol";
+import {ERC8183} from "../contracts/ERC8183.sol";
 
-/// @notice Broadcasts an ERC1967 proxy pointing at ERC8183WithAuthorization.
+/// @notice Broadcasts an ERC1967 proxy pointing at ERC8183.
 /// @dev Set TREASURY and ADMIN. The broadcast signer pays gas and does not need to be ADMIN
 ///      unless PAYMENT_TOKEN is set (allowlist requires ADMIN_ROLE).
 contract Deploy is Script {
@@ -16,10 +16,13 @@ contract Deploy is Script {
 
         vm.startBroadcast();
 
-        ERC8183WithAuthorization impl = new ERC8183WithAuthorization();
-        bytes memory initData = abi.encodeCall(ERC8183WithAuthorization.initialize, (treasury, admin));
+        ERC8183 impl = new ERC8183();
+        bytes memory initData = abi.encodeCall(
+            ERC8183.initialize,
+            (treasury, admin)
+        );
         ERC1967Proxy proxy = new ERC1967Proxy(address(impl), initData);
-        ERC8183WithAuthorization core = ERC8183WithAuthorization(address(proxy));
+        ERC8183 core = ERC8183(address(proxy));
 
         if (paymentToken != address(0)) {
             core.setPaymentTokenAllowed(paymentToken, true);
